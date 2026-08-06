7a22f784ed7aceb49e8d8befffa7d600e629145b
Natan Hari Pamungkas <natanhp@protonmail.com>
2026-08-06 15:13:54 +0700

    chore: remove untracked .astro cache and obsolete tailwind config
    - Stop tracking and remove the .astro build directory and dev.json state file
    - Delete obsolete tailwind.config.mjs as Tailwind v4 uses CSS-first configuration

--- .astro/content-modules.mjs
+++
@@ -1,6 +0,0 @@
-
-export default new Map([
-["src/blogs/flare-on-11-1-frog.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fblogs%2Fflare-on-11-1-frog.mdx&astroContentModuleFlag=true")],
-["src/blogs/gereja-orthodox-solo-sejarah-gereja-orthodox-timur-pertama-di-indonesia.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fblogs%2Fgereja-orthodox-solo-sejarah-gereja-orthodox-timur-pertama-di-indonesia.mdx&astroContentModuleFlag=true")],
-["src/blogs/renungan_puasa_bagi_anak_muda.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fblogs%2Frenungan_puasa_bagi_anak_muda.mdx&astroContentModuleFlag=true")]]);
-		
